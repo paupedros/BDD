c86dab54b83138508b228bec365c622238d4c018
Pau Pedros <paupedros10@gmail.com>
2023-11-29 09:00:14 +0100

    update

--- sql/exemples/file5.sql
+++ sql/exemples/file5.sql
@@ -1,5 +1,23 @@
-xsCREATE TABLE t6 (
+-- Forma malament dona error
+CREATE TABLE t6 (
 	nom_hotel varchar(30) PRIMARY KEY,
 	num_habitacio smallint primary key,
 	tipus varchar(20)
 );
+
+-- Forma correcte
+CREATE TABLE t6 (
+	nom_hotel varchar(30),
+	num_habitacio smallint,
+	tipus varchar(20),
+	CONSTRAINT pk_t6 primary key(nom_hotel, num_habitacio)
+);
+
+CREATE TABLE persona(
+	codi int auto_increment,
+	sou float not null,
+	CONSTRAINT pk_persona primary key(codi),
+	CONSTRAINT ch_sou check(sou>400)
+);
+
+
